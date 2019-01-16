44970754f5b02032fc37a1d3fb1f5440a2982806
ogwurujohnson <ogwurujohnson@gmail.com>
2019-01-16 13:18:40 +0100

    add home route

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const bodyParser = require(body-parser);
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req,res) => {
   res.status(200).send('welcome traveller');
 });
 
-
+app.post('/login',)
 
 
 const port = 3000 || process.env.PORT;
